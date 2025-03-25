6243e1215afc49061e44873a66ff5c93689a4354
Xinliang Chen <92891438+Tim-Chen11@users.noreply.github.com>
2025-03-25 01:07:31 +0000

    stack queue

---
+++ StackQueue/20.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> deque = new LinkedList<>();
+        char ch;
+        for (int i = 0; i < s.length(); i++) {
+            ch = s.charAt(i);
+
+            if (ch == '(') {
+                deque.push(')');
+            }else if (ch == '{') {
+                deque.push('}');
+            }else if (ch == '[') {
+                deque.push(']');
+            } else if (deque.isEmpty() || deque.peek() != ch) {
+                return false;
+            }else {//如果是右括号判断是否和栈顶元素匹配
+                deque.pop();
+            }
+        }
+        //遍历结束，如果栈为空，则括号全部匹配
+        return deque.isEmpty();
+    }
+}
