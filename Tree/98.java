65344f8f5bf59975f0cea312e1c7923ad25a4dc2
Xinliang Chen <92891438+Tim-Chen11@users.noreply.github.com>
2025-06-14 07:17:53 +0000

    dsa

---
+++ Tree/98.java
@@ -0,0 +1,16 @@
+class Solution {
+    private long prev = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (root.val <= prev) { // 不满足二叉搜索树条件
+            return false;
+        }
+        prev = root.val;
+        return isValidBST(root.right);
+    }
+}
