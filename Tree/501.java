cd9785af610a92bc796eca12bda8824f1395b54a
Xinliang Chen <92891438+Tim-Chen11@users.noreply.github.com>
2025-06-17 17:34:52 +0000

    dsa

---
+++ Tree/501.java
@@ -0,0 +1,43 @@
+class Solution {
+    ArrayList<Integer> resList;
+    int maxCount;
+    int count;
+    TreeNode pre;
+
+    public int[] findMode(TreeNode root) {
+        resList = new ArrayList<>();
+        maxCount = 0;
+        count = 0;
+        pre = null;
+        findMode1(root);
+        int[] res = new int[resList.size()];
+        for (int i = 0; i < resList.size(); i++) {
+            res[i] = resList.get(i);
+        }
+        return res;
+    }
+
+    public void findMode1(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        findMode1(root.left);
+
+        int rootValue = root.val;
+        if (pre == null || rootValue != pre.val) {
+            count = 1;
+        } else {
+            count++;
+        }
+        if (count > maxCount) {
+            resList.clear();
+            resList.add(rootValue);
+            maxCount = count;
+        } else if (count == maxCount) {
+            resList.add(rootValue);
+        }
+        pre = root;
+
+        findMode1(root.right);
+    }
+}
